21f6a2060d93890644ce157663a82debc8cdc7be
Crowdbotics <team@crowdbotics.com>
2021-09-19 15:16:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "saibaba_30681Identifier",
+  url: "https://saibaba_30681Identifier.botics.co",
+  api: "https://saibaba_30681Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
